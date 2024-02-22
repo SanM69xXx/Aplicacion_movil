2888c0c713a16a6a7d110eb0c24552b0cea401c6
SaNtiagO muñox <129355209+SanM69xXx@users.noreply.github.com>
2024-02-21 23:59:07 -0500

    textos ya creados en la segunda pantalla

--- app/src/main/java/com/sam/aplicacion_movil/segunda_pantalla.java
+++ app/src/main/java/com/sam/aplicacion_movil/segunda_pantalla.java
@@ -3,14 +3,19 @@ package com.sam.aplicacion_movil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class segunda_pantalla extends AppCompatActivity {
+
+    TextView texto2,texto3;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_pantalla);
 
-        
+
     }
 }
