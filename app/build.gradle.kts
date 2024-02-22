708eb2650c03b5b9c2bfb4142df6a943a9001de3
SaNtiagO muñox <129355209+SanM69xXx@users.noreply.github.com>
2024-02-22 09:01:36 -0500

    agregue implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    def lottieVersion = "6.3.0"
-    implementation "com.airbnb.android:lottie:$lottieVersion"
+    implementation ("com.airbnb.android:lottie:6.3.0")
 }
