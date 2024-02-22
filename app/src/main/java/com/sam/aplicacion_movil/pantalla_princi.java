95884beda1e0be0df318f3949555560ef6e3059a
SaNtiagO muñox <129355209+SanM69xXx@users.noreply.github.com>
2024-02-22 09:50:49 -0500

    aplicacion con logo

--- app/src/main/java/com/sam/aplicacion_movil/pantalla_princi.java
+++ app/src/main/java/com/sam/aplicacion_movil/pantalla_princi.java
@@ -2,7 +2,9 @@ package com.sam.aplicacion_movil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class pantalla_princi extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class pantalla_princi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_princi);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(pantalla_princi.this, MainActivity.class));
+                finish();
+            }
+        },2000);
     }
 }
